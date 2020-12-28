93be1d15ae2587e1f3a3dce8964420476a72b0f5
camacleod527 <carolyn@melaniecasey.com>
2020-12-28 18:01:38 -0500

    added weather API and JS

---
+++ src/project.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "2e28a98ce91091353977894a6f91ca64";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
