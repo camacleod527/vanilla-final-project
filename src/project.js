d0996a464a85d0059f9e56f2a7dea70a1de6814e
camacleod527 <carolyn@melaniecasey.com>
2020-12-29 09:15:36 -0500

    changed api url

--- src/project.js
+++ src/project.js
@@ -29,6 +29,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "2e28a98ce91091353977894a6f91ca64";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city ="Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
