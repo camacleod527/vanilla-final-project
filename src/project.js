2eceb58327863cc690bd31d6715afac155b06d9f
camacleod527 <carolyn@melaniecasey.com>
2020-12-30 10:46:58 -0500

    Added search engine JS functionality

--- src/project.js
+++ src/project.js
@@ -35,8 +35,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "2e28a98ce91091353977894a6f91ca64";
-let city ="Boston";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "2e28a98ce91091353977894a6f91ca64";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value)
+}
+ 
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
